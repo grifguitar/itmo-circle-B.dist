5a866c3e96846084064ce2a4b880ac8f84e7d442
Grigoriy Khlytin <xlootin@niuitmo.ru>
2021-10-14 04:39:22 +0300

    added materials of the first lecture

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Grigoriy Khlytin on 14.10.2021.
+//
+
+#include "lecture1_graphs_dfs/CycleSearch.h"
+
+int main() {
+
+    //используем наш класс, который будет искать цикл
+    CycleSearch cycleSearch;
+
+    //запустим решение
+    cycleSearch.solve();
+
+    return 0;
+}
